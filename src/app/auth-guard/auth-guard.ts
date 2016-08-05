049d00e55c07e06c10a087abbe78e1a1efbb8129
ahmetesat <ahmetkara@std.sehir.edu.tr>
2016-08-05 08:15:26 +0300

    routing fixed

--- src/app/auth-guard/auth-guard.ts
+++ src/app/auth-guard/auth-guard.ts
@@ -1,10 +1,20 @@
-import { CanActivate }    from '@angular/router';
+import { CanActivate, Router }    from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
 
   canActivate() {
     console.log('AuthGuard#canActivate called');
+    //return false;
+    if (localStorage.getItem('access_token') !== null) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
     return false;
+
   }
 
 }
