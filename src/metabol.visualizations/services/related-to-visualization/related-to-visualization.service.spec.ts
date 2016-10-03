e4df2f567fddcd644a7ca643f0f2255d11135d2c
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-10-03 19:22:34 +0300

    visualization fixes

--- src/metabol.visualizations/services/related-to-visualization/related-to-visualization.service.spec.ts
+++ src/metabol.visualizations/services/related-to-visualization/related-to-visualization.service.spec.ts
@@ -11,6 +11,19 @@ describe('RelatedToVisualization Service', () => {
 
   let service: RelatedToVisualizationService;
 
+  let localStoreData = {
+    'h[c]': '',
+    'm1': ''
+  };
+
+  /**
+   * Bad hacking beacuse dependcy injection not working
+   */
+  beforeAll(function() {
+    localStorage.clear();
+    localStorage.setItem('currency-metabolites', JSON.stringify(localStoreData));
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -26,49 +39,105 @@ describe('RelatedToVisualization Service', () => {
     expect(service).toBeTruthy();
   });
 
-  let relatedMetabolites: RelatedMetabolite[] = [
+  let relatedMetabolites: RelatedMetabolite =
     <RelatedMetabolite>{
       id: 'm1', name: 'm1', stoichiometry: 1,
       reactions: [
-        { id: "r2", name: 'r2', stoichiometry: 1 },
-        { id: "r4", name: 'r4', stoichiometry: -1 }]
-    },
-    <RelatedMetabolite>{
-      id: 'h[c]', name: 'h[c]', stoichiometry: 2,
-      reactions: [{ id: "r3", name: 'r3', stoichiometry: -1 }]
-    },
-    <RelatedMetabolite>{
-      id: 'm3', name: 'm3', stoichiometry: -1,
-      reactions: []
-    },
-  ];
+        { id: "r2", name: 'r2', stoichiometry: 1, subsystem: 's1' },
+        {
+          id: "r4", name: 'r4', stoichiometry: -1, subsystem: 's2',
+          metabolites: [
+            <RelatedMetabolite>{
+              id: 'h[c]', name: 'h[c]', stoichiometry: 2,
+              reactions: [{ id: "r3", name: 'r3', stoichiometry: -1 }]
+            },
+            <RelatedMetabolite>{
+              id: 'm3', name: 'm3', stoichiometry: -1,
+              reactions: [
+                { id: 'r5', name: 'r5', subsystem: 's3' }
+              ]
+            }
+          ]
+        }
+      ]
+    };
 
 
 
   it('visual related reactions', () => {
 
-    let expectedVisual = [
-      [
-        { name: 'm1', type: 'm' },
-        { name: 'r2', type: 'r' },
-        { name: 'r4', type: 'r' }
-      ],
-      [
-        {
-          source: { name: 'r2', type: 'r' },
-          target: { name: 'm1', type: 'm' },
-          role: 'p', stoichiometry: 1
-        },
-        {
-          source: { name: 'r4', type: 'r' },
-          target: { name: 'm1', type: 'm' },
-          role: 'p', stoichiometry: 1
-        }
-      ]
+    let expectedNodes = [
+      { name: 's1', type: 'sub', reactions: [{ name: 'r2', type: 'r' }] },
+      { name: 's2', type: 'sub', reactions: [{ name: 'r4', type: 'r' }] },
+      { name: 's3', type: 'sub', reactions: [{ name: 'r5', type: 'r' }] },
+      { name: 'm1', type: 'm' },
+      { name: 'h[c]', type: 'm' },
+      { name: 'm3', type: 'm' },
+      { name: 'r2', type: 'r' },
+      { name: 'r4', type: 'r' },
+      { name: 'r5', type: 'r' },
     ];
 
-    let visual = service.visualizeRelatedMetabolites(relatedMetabolites[0]);
-    expect(expectedVisual).toEqual(visual);
+    let expectedLinks = [
+      {
+        source: expectedNodes[0],
+        target: expectedNodes[3],
+        role: 'sub'
+      },
+      {
+        source: expectedNodes[1],
+        target: expectedNodes[3],
+        role: 'sub'
+      },
+      {
+        source: { name: 'r2', type: 'r' },
+        target: { name: 'm1', type: 'm' },
+        role: 'p', stoichiometry: 1
+      },
+      {
+        source: { name: 'r4', type: 'r' },
+        target: { name: 'm1', type: 'm' },
+        role: 'p', stoichiometry: 1
+      },
+      {
+        source: { name: 'r4', type: 'r' },
+        target: { name: 'h[c]', type: 'm' },
+        role: 'p', stoichiometry: 2
+      },
+      {
+        source: { name: 'm3', type: 'm' },
+        target: { name: 'r4', type: 'r' },
+        role: 's', stoichiometry: -1
+      },
+      {
+        source: expectedNodes[2],
+        target: { name: 'm3', type: 'm' },
+        role: 'sub'
+      },
+      {
+        source: { name: 'm3', type: 'm' },
+        target: { name: 'r5', type: 'r' },
+        role: 's', stoichiometry: -1
+      },
+    ];
+
+    let visual = service.visualizeRelatedMetabolites(relatedMetabolites);
+
+    expect(expectedNodes.length).toEqual(visual[0].length);
+    expect(expectedLinks.length).toEqual(visual[1].length);
+
+    for (let i = 0; i < expectedNodes.length; i++) {
+      expect(expectedNodes[i]).toEqual(visual[0][i]);
+    }
+
+    for (let i = 0; i < expectedLinks.length; i++) {
+      expect(expectedLinks[i]).toEqual(visual[1][i]);
+    }
+
+  });
+
+  afterAll(() => {
+    localStorage.clear();
   });
 
 });
