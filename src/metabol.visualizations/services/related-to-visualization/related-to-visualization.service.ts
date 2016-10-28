403ab0579ab9ef5bccfeec1e62354ebdc64c6705
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-10-28 19:55:00 +0300

    subsytem analyze

--- src/metabol.visualizations/services/related-to-visualization/related-to-visualization.service.ts
+++ src/metabol.visualizations/services/related-to-visualization/related-to-visualization.service.ts
@@ -44,6 +44,11 @@ export class RelatedToVisualizationService {
    */
   private normalize() {
     this.links = this.links.filter(l => l.source && l.target);
+    this.links = this.links.filter(l => {
+      let s = <FbaNode>l.source;
+      let t = <FbaNode>l.target;
+      return s.name != 'mock-metabolite' && t.name != 'mock-metabolite';
+    });
   }
 
   /**
@@ -64,6 +69,16 @@ export class RelatedToVisualizationService {
   private createMetaboliteIfDoNotExits(related: RelatedMetabolite) {
     if (!(related.id in this.metabolites))
       this.metabolites[related.id] = { name: related.id, type: 'm' };
+  }
+
+  vizulizeSubsystemDetail(relateds: RelatedReaction[]) {
+    let relatedMetabolite: RelatedMetabolite = {
+      id: 'mock-metabolite',
+      name: 'mock-metabolite',
+      stoichiometry: relateds[0].stoichiometry,
+      reactions: relateds
+    };
+    return this.visualizeRelatedMetabolites(relatedMetabolite);
   }
 
   /**
