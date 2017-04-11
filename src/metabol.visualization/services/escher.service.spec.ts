f0e361087f638dfbff8fec14d610d753a60f39dd
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-04-11 18:36:09 +0300

    test fix

--- src/metabol.visualization/services/escher.service.spec.ts
+++ src/metabol.visualization/services/escher.service.spec.ts
@@ -2,10 +2,13 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { EscherService } from './escher.service';
+import { HttpModule } from '@angular/http';
+
 
 describe('Service: Escher', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [EscherService]
     });
   });
