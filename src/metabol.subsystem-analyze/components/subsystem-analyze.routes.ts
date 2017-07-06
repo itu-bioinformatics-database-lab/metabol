5c9630db5c09d04db703c5375c79a7b397cd32c3
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-07-06 14:56:50 +0300

    menu order changed

--- src/metabol.subsystem-analyze/components/subsystem-analyze.routes.ts
+++ src/metabol.subsystem-analyze/components/subsystem-analyze.routes.ts
@@ -1,15 +1,19 @@
-import {SampleComponent} from "./sample/sample.component";
+import { SampleComponent } from "./sample/sample.component";
 import { Routes } from '@angular/router';
-import {ConcentrationTableComponent} from './concentration-table';
-import {ManualComponent} from './manual/manual.component';
-import {UploadComponent} from './upload/upload.component';
-import {SubsystemAnalyzeComponent} from './subsystem-analyze.component';
-import {MeasurementComponent} from './measurement/measurement.component';
+
+import { AuthGuard } from "../../metabol.auth";
+
+import { ConcentrationTableComponent } from './concentration-table';
+import { ManualComponent } from './manual/manual.component';
+import { UploadComponent } from './upload/upload.component';
+import { SubsystemAnalyzeComponent } from './subsystem-analyze.component';
+import { MeasurementComponent } from './measurement/measurement.component';
 
 export const SubsystemAnalyzeRoutes: Routes = [
   {
     path: 'analyze',
     component: SubsystemAnalyzeComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: 'manual', component: ManualComponent },
       { path: 'upload', component: UploadComponent },
@@ -17,6 +21,5 @@ export const SubsystemAnalyzeRoutes: Routes = [
       { path: 'sample', component: SampleComponent },
       { path: '', redirectTo: 'measurement', pathMatch: 'full' }
     ]
-  },
-
+  }
 ];
